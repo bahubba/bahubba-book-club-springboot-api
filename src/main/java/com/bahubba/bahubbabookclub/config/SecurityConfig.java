5bc45fe220ff60511807c17ddab97526474764b5
bahubba <bahubba89@gmail.com>
2023-06-15 21:35:41 -0400

    BAHubba-CU-85zt8zz0j: Update Spring Boot

--- src/main/java/com/bahubba/bahubbabookclub/config/SecurityConfig.java
+++ src/main/java/com/bahubba/bahubbabookclub/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -21,26 +23,22 @@ public class SecurityConfig {
 
     private final JWTAuthenticationFilter jwtAuthFilter;
 
-    // TODO - A lot of what's below is deprecated by Spring Security 6.1,
-    //        which is a dependency of the latest Spring Boot 3.1...
-    //        Look into how to update
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-            .csrf().disable()
-            .authorizeHttpRequests()
-            .requestMatchers(
-                "/v3/api-docs",
-                "/v3/api-docs/**",
-                "/swagger-ui/**",
-                "/api/v1/auth/**"
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement((sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests((authorizeRequests) ->
+                authorizeRequests.requestMatchers(
+                    "/v3/api-docs",
+                    "/v3/api-docs/**",
+                    "/swagger-ui/**",
+                    "/api/v1/auth/**"
+                )
+                .permitAll()
+                .anyRequest()
+                .authenticated()
             )
-            .permitAll()
-            .anyRequest()
-            .authenticated()
-            .and()
-            .sessionManagement()
-            .and()
             .authenticationProvider(authenticationProvider)
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
             .cors(cors -> cors.configurationSource(corsConfigurationSource()));
