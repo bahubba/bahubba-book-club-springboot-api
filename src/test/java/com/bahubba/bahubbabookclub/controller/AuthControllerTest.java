b9aace0bad859862eae03fae5b7b6459642fa545
bahubba <bahubba89@gmail.com>
2023-07-05 19:42:46 -0400

    BAHubba-CU-85zt7zwn5: Unit Testing
    WIP

--- src/test/java/com/bahubba/bahubbabookclub/controller/AuthControllerTest.java
+++ src/test/java/com/bahubba/bahubbabookclub/controller/AuthControllerTest.java
@@ -1,21 +1,29 @@
 package com.bahubba.bahubbabookclub.controller;
 
 import com.bahubba.bahubbabookclub.model.dto.AuthDTO;
+import com.bahubba.bahubbabookclub.model.dto.MessageResponseDTO;
 import com.bahubba.bahubbabookclub.model.dto.ReaderDTO;
 import com.bahubba.bahubbabookclub.model.payload.AuthRequest;
 import com.bahubba.bahubbabookclub.model.payload.NewReader;
 import com.bahubba.bahubbabookclub.service.AuthService;
+import com.bahubba.bahubbabookclub.service.JwtService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
+/**
+ * Unit tests for the authentication endpoints
+ */
 @SpringBootTest
 class AuthControllerTest {
 
@@ -24,6 +32,9 @@ class AuthControllerTest {
 
     @MockBean
     AuthService authService;
+
+    @MockBean
+    JwtService jwtService;
 
     @Test
     void testRegisterUser() {
@@ -57,5 +68,45 @@ class AuthControllerTest {
         verify(authService, times(1)).authenticate(any(AuthRequest.class));
         assertThat(rsp).isNotNull();
         assertThat(rsp.getBody()).isNotNull();
+    }
+
+    @Test
+    void testRefreshToken() {
+        when(jwtService.getJwtRefreshFromCookies(any(HttpServletRequest.class))).thenReturn("sometoken");
+        when(jwtService.refreshToken(anyString())).thenReturn(
+            ResponseEntity.ok().body(MessageResponseDTO.builder().message("ok").build())
+        );
+
+        ResponseEntity<MessageResponseDTO> rsp = authController.refreshToken(new MockHttpServletRequest());
+
+        verify(jwtService, times(1)).getJwtRefreshFromCookies(any(HttpServletRequest.class));
+        verify(jwtService, times(1)).refreshToken(anyString());
+        assertThat(rsp.getBody()).isNotNull();
+    }
+
+    @Test
+    void testRefreshToken_nullRefreshToken() {
+        when(jwtService.getJwtRefreshFromCookies(any(HttpServletRequest.class))).thenReturn(null);
+
+        ResponseEntity<MessageResponseDTO> rsp = authController.refreshToken(new MockHttpServletRequest());
+
+        verify(jwtService, times(1)).getJwtRefreshFromCookies(any(HttpServletRequest.class));
+        verify(jwtService, times(0)).refreshToken(anyString());
+        assertThat(rsp.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        assertThat(rsp.getBody()).isNotNull();
+        assertThat("Refresh token empty").isEqualTo(rsp.getBody().getMessage());
+    }
+
+    @Test
+    void testRefreshToken_emptyRefreshToken() {
+        when(jwtService.getJwtRefreshFromCookies(any(HttpServletRequest.class))).thenReturn("");
+
+        ResponseEntity<MessageResponseDTO> rsp = authController.refreshToken(new MockHttpServletRequest());
+
+        verify(jwtService, times(1)).getJwtRefreshFromCookies(any(HttpServletRequest.class));
+        verify(jwtService, times(0)).refreshToken(anyString());
+        assertThat(rsp.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        assertThat(rsp.getBody()).isNotNull();
+        assertThat("Refresh token empty").isEqualTo(rsp.getBody().getMessage());
     }
 }
