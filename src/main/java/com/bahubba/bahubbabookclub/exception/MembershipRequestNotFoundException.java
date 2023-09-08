300d49c1fcb00c1bc752c5ec136d5209275d9e67
bahubba89@gmail.com <bahubba89@gmail.com>
2023-09-08 18:31:59 -0400

    BAHubba-CU-86ay5yt3w_AdminMembershipRequests

--- src/main/java/com/bahubba/bahubbabookclub/exception/MembershipRequestNotFoundException.java
+++ src/main/java/com/bahubba/bahubbabookclub/exception/MembershipRequestNotFoundException.java
@@ -3,8 +3,6 @@ package com.bahubba.bahubbabookclub.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.UUID;
-
 /**
  * Custom exception for when a membership request that doesn't exist
  */
